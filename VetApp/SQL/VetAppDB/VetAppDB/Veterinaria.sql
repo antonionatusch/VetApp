d040fd09a0ebf57c8e883612df0ed8dc089297f0
Antonio Natusch <a2022111958@estudiantes.upsa.edu.bo>
2024-06-06 16:36:54 -0400

    Se agregaron los primeros modelos de la base de datos, peronas, clientes y su entidad asociativa.

--- VetApp/SQL/VetAppDB/VetAppDB/Veterinaria.sql
+++ VetApp/SQL/VetAppDB/VetAppDB/Veterinaria.sql
@@ -1,3 +1,55 @@
--- probando merge
+CREATE DATABASE VeterinariaExtendida
+USE VeterinariaExtendida
 
--- comentario extra
+-- sin foreign keys: persona, cliente, vacuna, servicio, hospedaje, alimento, medicamento, comodidades
+
+/*
+tipos custom
+*/
+
+CREATE TYPE idFijo FROM char(20) not null
+CREATE TYPE nombre FROM varchar(80) not null
+CREATE TYPE numTel FROM varchar(50) not null
+CREATE TYPE correo FROM varchar(50) not null
+CREATE TYPE direccion FROM varchar(60) not null
+CREATE TYPE fechaObligatoria FROM date not null
+
+CREATE TABLE Personas
+(
+
+	ci				idFijo,
+	nombre			nombre, 
+	telefono		numTel,
+	correo			correo,
+	direccion		direccion
+
+	CONSTRAINT PK_Personas PRIMARY KEY (ci)
+
+);
+
+
+CREATE TABLE Clientes
+(
+
+	codCliente		idFijo,
+	apellido		nombre,
+	cuentaBanco		varchar(40) not null,
+	banco			nombre,
+	direccion		direccion,
+	telefono		numTel,
+	correo			correo,
+	CONSTRAINT PK_Clientes PRIMARY KEY (codCliente)
+
+);
+
+CREATE TABLE PersonaCliente
+(
+	codCliente			idFijo,
+	ci					idFijo,
+	fechaAsociacion		fechaObligatoria
+
+	CONSTRAINT PK_PC PRIMARY KEY (codCliente, ci, fechaAsociacion),
+	CONSTRAINT FK_ClientesPC FOREIGN KEY (codCliente) REFERENCES Clientes(codCliente),
+	CONSTRAINT FK_PersonasPC FOREIGN KEY (ci) REFERENCES Personas(ci)
+);
+
