36648db98bfbc048561c0ec738c5bead6c1a29f7
Antonio Natusch <a2022111958@estudiantes.upsa.edu.bo>
2024-06-08 16:27:09 -0400

    added basic crud functionality without stored procedures. todo: use stored procedure for insertion

---
+++ VetApp/Controllers/ConsumosVetController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VetApp.Models;
+
+namespace VetApp.Controllers
+{
+    public class ConsumosVetController : Controller
+    {
+        private readonly VeterinariaExtendidaContext _context;
+
+        public ConsumosVetController(VeterinariaExtendidaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ConsumosVet
+        public async Task<IActionResult> Index()
+        {
+            var consumosVets = await _context.ConsumosVets
+                .Include(c => c.CodMascotaNavigation)
+                .Include(c => c.CodVacunaNavigation)
+                .Include(c => c.IdServicioNavigation)
+                .ToListAsync();
+            return View(consumosVets);
+        }
+
+        // GET: ConsumosVet/Details
+        public async Task<IActionResult> Details(string codMascota, string idServicio, int idConsumoVet)
+        {
+            if (codMascota == null || idServicio == null || idConsumoVet == 0)
+            {
+                return NotFound();
+            }
+
+            var consumosVet = await _context.ConsumosVets
+                .Include(c => c.CodMascotaNavigation)
+                .Include(c => c.CodVacunaNavigation)
+                .Include(c => c.IdServicioNavigation)
+                .FirstOrDefaultAsync(m => m.CodMascota == codMascota && m.IdServicio == idServicio && m.IdConsumoVet == idConsumoVet);
+
+            if (consumosVet == null)
+            {
+                return NotFound();
+            }
+
+            return View(consumosVet);
+        }
+
+        // GET: ConsumosVet/Create
+        public IActionResult Create()
+        {
+            ViewData["CodMascota"] = new SelectList(_context.Mascotas, "CodMascota", "Nombre");
+            ViewData["CodVacuna"] = new SelectList(_context.Vacunas, "CodVacuna", "Nombre");
+            ViewData["IdServicio"] = new SelectList(_context.Servicios, "IdServicio", "Nombre");
+            return View();
+        }
+
+        // POST: ConsumosVet/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CodMascota,CodVacuna,IdServicio,Observaciones,CantVacunas,Nit")] ConsumosVet consumosVet)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(consumosVet);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CodMascota"] = new SelectList(_context.Mascotas, "CodMascota", "Nombre", consumosVet.CodMascota);
+            ViewData["CodVacuna"] = new SelectList(_context.Vacunas, "CodVacuna", "Nombre", consumosVet.CodVacuna);
+            ViewData["IdServicio"] = new SelectList(_context.Servicios, "IdServicio", "Nombre", consumosVet.IdServicio);
+            return View(consumosVet);
+        }
+
+        // GET: ConsumosVet/Edit
+        public async Task<IActionResult> Edit(string codMascota, string idServicio, int idConsumoVet)
+        {
+            if (codMascota == null || idServicio == null || idConsumoVet == 0)
+            {
+                return NotFound();
+            }
+
+            var consumosVet = await _context.ConsumosVets.FindAsync(codMascota, idServicio, idConsumoVet);
+            if (consumosVet == null)
+            {
+                return NotFound();
+            }
+            ViewData["CodMascota"] = new SelectList(_context.Mascotas, "CodMascota", "Nombre", consumosVet.CodMascota);
+            ViewData["CodVacuna"] = new SelectList(_context.Vacunas, "CodVacuna", "Nombre", consumosVet.CodVacuna);
+            ViewData["IdServicio"] = new SelectList(_context.Servicios, "IdServicio", "Nombre", consumosVet.IdServicio);
+            return View(consumosVet);
+        }
+
+        // POST: ConsumosVet/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string codMascota, string idServicio, int idConsumoVet, [Bind("CodMascota,CodVacuna,IdServicio,Observaciones,CantVacunas,Nit")] ConsumosVet consumosVet)
+        {
+            if (codMascota != consumosVet.CodMascota || idServicio != consumosVet.IdServicio || idConsumoVet != consumosVet.IdConsumoVet)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(consumosVet);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ConsumosVetExists(consumosVet.CodMascota, consumosVet.IdServicio, consumosVet.IdConsumoVet))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CodMascota"] = new SelectList(_context.Mascotas, "CodMascota", "Nombre", consumosVet.CodMascota);
+            ViewData["CodVacuna"] = new SelectList(_context.Vacunas, "CodVacuna", "Nombre", consumosVet.CodVacuna);
+            ViewData["IdServicio"] = new SelectList(_context.Servicios, "IdServicio", "Nombre", consumosVet.IdServicio);
+            return View(consumosVet);
+        }
+
+        // GET: ConsumosVet/Delete
+        public async Task<IActionResult> Delete(string codMascota, string idServicio, int idConsumoVet)
+        {
+            if (codMascota == null || idServicio == null || idConsumoVet == 0)
+            {
+                return NotFound();
+            }
+
+            var consumosVet = await _context.ConsumosVets
+                .Include(c => c.CodMascotaNavigation)
+                .Include(c => c.CodVacunaNavigation)
+                .Include(c => c.IdServicioNavigation)
+                .FirstOrDefaultAsync(m => m.CodMascota == codMascota && m.IdServicio == idServicio && m.IdConsumoVet == idConsumoVet);
+
+            if (consumosVet == null)
+            {
+                return NotFound();
+            }
+
+            return View(consumosVet);
+        }
+
+        // POST: ConsumosVet/Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string codMascota, string idServicio, int idConsumoVet)
+        {
+            var consumosVet = await _context.ConsumosVets.FindAsync(codMascota, idServicio, idConsumoVet);
+            if (consumosVet != null)
+            {
+                _context.ConsumosVets.Remove(consumosVet);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ConsumosVetExists(string codMascota, string idServicio, int idConsumoVet)
+        {
+            return _context.ConsumosVets.Any(e => e.CodMascota == codMascota && e.IdServicio == idServicio && e.IdConsumoVet == idConsumoVet);
+        }
+    }
+}
