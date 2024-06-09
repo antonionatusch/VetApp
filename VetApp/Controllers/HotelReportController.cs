e045e2c78cf6c89eb0f759528a10ecb6d6365d09
Antonio Natusch <a2022111958@estudiantes.upsa.edu.bo>
2024-06-09 19:14:23 -0400

    finished all business logic, will style later

---
+++ VetApp/Controllers/HotelReportController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using VetApp.Models;
+
+namespace VetApp.Controllers
+{
+    public class HotelReportController : Controller
+    {
+        private readonly VeterinariaExtendidaContext _context;
+
+        public HotelReportController(VeterinariaExtendidaContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult GenerateHotelReport()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GenerateHotelReport(DateOnly fechaInicio, DateOnly fechaFin)
+        {
+            var reportResults = new List<HotelConsumptionReportViewModel>();
+
+            using (var command = _context.Database.GetDbConnection().CreateCommand())
+            {
+                command.CommandText = "GenerateHotelConsumptionReport";
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.Add(new SqlParameter("@fechaInicio", fechaInicio));
+                command.Parameters.Add(new SqlParameter("@fechaFin", fechaFin));
+
+                var resultadoParam = new SqlParameter("@resultado", SqlDbType.Int)
+                {
+                    Direction = ParameterDirection.Output
+                };
+                command.Parameters.Add(resultadoParam);
+
+                await _context.Database.OpenConnectionAsync();
+
+                using (var result = await command.ExecuteReaderAsync())
+                {
+                    while (await result.ReadAsync())
+                    {
+                        reportResults.Add(new HotelConsumptionReportViewModel
+                        {
+                            CodMascota = result.GetString(0),
+                            NombreMascota = result.GetString(1),
+                            Cliente = result.GetString(2),
+                            IdServicio = result.GetString(3),
+                            NombreServicio = result.GetString(4),
+                            Observaciones = result.GetString(5),
+                            NochesHosp = result.GetInt32(6),
+                            CantidadAlim = result.GetInt32(7),
+                            CantidadMedic = result.GetInt32(8),
+                            CantidadCom = result.GetInt32(9),
+                            Nit = result.GetString(10),
+                            Fecha = DateOnly.FromDateTime(result.GetDateTime(11)),
+                            PrecioTotal = result.GetDecimal(12),
+                            PrecioTotalGeneral = result.IsDBNull(13) ? (decimal?)null : result.GetDecimal(13)
+                        });
+                    }
+                }
+
+                await _context.Database.CloseConnectionAsync();
+                // Obtén el valor del parámetro de salida
+                var resultado = (int)resultadoParam.Value;
+                if (resultado != 1)
+                {
+                    ModelState.AddModelError("", "Error generating report.");
+                    return View();
+                }
+            }
+
+            return View("HotelReportResults", reportResults);
+        }
+    }
+}
---
+++ VetApp/Models/HotelConsumptionReportViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace VetApp.Models
+{
+    public class HotelConsumptionReportViewModel
+    {
+        public string CodMascota { get; set; }
+        public string NombreMascota { get; set; }
+        public string Cliente { get; set; }
+        public string IdServicio { get; set; }
+        public string NombreServicio { get; set; }
+        public string Observaciones { get; set; }
+        public int NochesHosp { get; set; }
+        public int CantidadAlim { get; set; }
+        public int CantidadMedic { get; set; }
+        public int CantidadCom { get; set; }
+        public string Nit { get; set; }
+        public DateOnly Fecha { get; set; }
+        public decimal PrecioTotal { get; set; }
+        public decimal? PrecioTotalGeneral { get; set; }
+    }
+}
