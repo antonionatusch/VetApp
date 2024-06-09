3cf6f58a26272185b719cefd4e7e2f5b245a07b1
Antonio Natusch <a2022111958@estudiantes.upsa.edu.bo>
2024-06-09 18:22:15 -0400

    testing default autogenerated views and controllers for extra consumptions, worked

--- VetApp/Models/Alimento.cs
+++ VetApp/Models/Alimento.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace VetApp.Models;
 
 public partial class Alimento
 {
+    [Display(Name ="ID del alimento")]
     public int CodAlimento { get; set; }
+    [Display(Name = "Nombre")]
 
     public string Nombre { get; set; } = null!;
+    [Display(Name = "Descripción")]
 
     public string Descripcion { get; set; } = null!;
 
     public string Proveedor { get; set; } = null!;
+    [Display(Name = "Precio unitario")]
 
     public decimal PrecioUnitario { get; set; }
 
